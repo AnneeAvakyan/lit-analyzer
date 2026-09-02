d4a1d055667a219cdfdabcc5deb577267e47dcbc
AnneeAvakyan <1AnnaAvakyan1@gmail.com>
2026-09-02 09:18:48 +0300

    add migrations for global_sentence_index

--- internal/domain/entities/mention.go
+++ internal/domain/entities/mention.go
@@ -1,9 +1,9 @@
 package entities
 
 type Mention struct {
-	ID            int `json:"id"`
-	CharacterID   int `json:"characterID"`
-	ChapterID     int `json:"chapterID"`
-	Position      int `json:"position"`
-	SentenceIndex int `json:"sentenceIndex"`
+	ID                  int `json:"id"`
+	CharacterID         int `json:"characterID"`
+	ChapterID           int `json:"chapterID"`
+	GlobalSentenceIndex int `json:"global_sentence_index"`
+	SentenceIndex       int `json:"sentenceIndex"`
 }
